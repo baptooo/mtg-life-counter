17bf3b3ef57676eb43ff258f356335022a6796d8
Baptiste Rios Campo <baptiste@cardiologs.com>
2023-05-30 18:59:04 +0200

    fix: layout and resize for multiple devices

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -1,23 +1,39 @@
+import { PropsWithChildren, useMemo, FC } from "react";
 import "./ColorPicker.css";
 import { CounterHook, MTGColor } from "./counter-hook";
 
-const COLORS: MTGColor[] = ["green", "white", "blue", "black", "red"];
-
-export const ColorPicker = (props: {
+type Props = PropsWithChildren<{
   counter: CounterHook;
   revert?: boolean;
-}) => {
+}>
+const COLORS: MTGColor[] = ["black", "red", "green", "white", "blue"];
+
+export const ColorPicker: FC<Props> = ({ revert, counter }) => {
+  const positions = useMemo(() => {
+    const len = 5;
+    const radius = 10;
+    const gap = Math.PI * 2 / len;
+    let offset = Math.PI * 0.3;
+
+    if (revert) offset += Math.PI;
+
+    return COLORS.map((_, idx) => {
+      const x = Math.cos(idx * gap + offset) * radius - 4;
+      const y = Math.sin(idx * gap + offset) * radius - 4;
+      return {x, y};
+    });
+  }, [revert]);
+
   return (
-    <div className={`colorpicker ${props.revert ? "revert" : ""}`}>
-      <div className="colorpicker-wrapper">
-        {COLORS.map((col) => (
-          <button
-            className={`color bg-${col}`}
-            key={col}
-            onClick={() => props.counter.setColor(col)}
-          />
-        ))}
-      </div>
+    <div className="colorpicker">
+      {COLORS.map((col, idx) => (
+        <button
+          style={{ transform: `translate3d(${positions[idx].x}vh, ${positions[idx].y}vh, 0)`}}
+          className={`color bg-${col}`}
+          key={col}
+          onClick={() => counter.setColor(col)}
+        />
+      ))}
     </div>
   );
 };
